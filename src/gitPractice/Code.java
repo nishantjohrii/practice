d2a0eb7332bf34a48e63ca0dd4cf50b67bdbb7ce
Nishant Johri <nishantjohrii@gmail.com>
2022-01-16 18:55:59 +0530

    second line added

--- src/gitPractice/Code.java
+++ src/gitPractice/Code.java
@@ -5,6 +5,8 @@ public class Code {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a first line of code");
+		
+		System.out.println("this is the second line of code");
 
 	}
 
